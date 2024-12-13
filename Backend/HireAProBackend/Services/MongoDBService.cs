e602d32f2784842dad50319b45628ce4b1595b6d
Klym-Sushelnytskyy Maxim <maxim.klym.ext@inetum.com>
2024-12-13 11:26:20 +0100

    Configurado con varialbes de entrono

--- Backend/HireAProBackend/Services/MongoDBService.cs
+++ Backend/HireAProBackend/Services/MongoDBService.cs
@@ -1,6 +1,7 @@
 ﻿using MongoDB.Driver;
 using Microsoft.Extensions.Options;
 using HireAProBackend.Models;
+using DotNetEnv;
 
 namespace HireAProBackend.Services
 {
@@ -11,11 +12,19 @@ namespace HireAProBackend.Services
         // Modificar para usar IOptions para acceder a las configuraciones
         public MongoDBService(IConfiguration configuration)
         {
-            var connectionString = configuration.GetValue<string>("MongoDBSettings:ConnectionString");
+            Env.Load();
+            var envMongoUser = Environment.GetEnvironmentVariable("mongo_admin");
+            var envMongoPasswd = Environment.GetEnvironmentVariable("mongo_passwd");
+
+            var escapedMongoUser = Uri.EscapeDataString(envMongoUser);
+            var escapedMongoPasswd = Uri.EscapeDataString(envMongoPasswd);
+
+            var connectionString = $"mongodb://{escapedMongoUser}:{escapedMongoPasswd}@148.113.191.24:27017";
+
             var databaseName = configuration.GetValue<string>("MongoDBSettings:DatabaseName");
 
             var client = new MongoClient(connectionString);
-            _database = client.GetDatabase(databaseName);
+            _database = client.GetDatabase("hire-a-pro");
         }
 
         public IMongoCollection<T> GetCollection<T>(string collectionName)
